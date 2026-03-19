043dbab6692f859708f639c2ceb1f8fd04810b3f
Adam Binnersley <abinnersley@gmail.com>
2026-03-19 20:55:59 +0000

    Update IPBlock.php

--- src/IPBlock.php
+++ src/IPBlock.php
@@ -18,6 +18,23 @@ class IPBlock
     protected $blocked_iso_countries = 'blocked_iso_countries';
 
     /**
+     * Verified search engine bot hostnames (used for reverse DNS verification)
+     * @var array
+     */
+    protected $verifiedBotDomains = [
+        '.googlebot.com',      // Googlebot
+        '.google.com',         // Google (other services)
+        '.search.msn.com',     // Bingbot
+        '.crawl.yahoo.net',    // Yahoo Slurp
+        '.yandex.ru',          // Yandex
+        '.yandex.net',         // Yandex
+        '.yandex.com',         // Yandex
+        '.crawl.baidu.com',    // Baidu
+        '.crawl.baidu.jp',     // Baidu
+        '.duckduckgo.com',     // DuckDuckGo
+    ];
+
+    /**
      * Adds a Database instance for the class to use
      * @param Database $db This should be an instance of the database connection
      */
@@ -26,7 +43,7 @@ class IPBlock
         $this->db = $db;
         $this->geoIP = new Reader(dirname(__FILE__).DIRECTORY_SEPARATOR.'Geo-Country.mmdb');
     }
-    
+
     /**
      * Change the default table name where the IP list is located
      * @param string $table This should be the name of the table where the list of IP are located
@@ -39,7 +56,7 @@ class IPBlock
         }
         return $this;
     }
-    
+
     /**
      * Returns the blocked IP's database table
      * @return string
@@ -61,7 +78,7 @@ class IPBlock
         }
         return $this;
     }
-    
+
     /**
      * Returns the blocked IP range database table
      * @return string
@@ -70,7 +87,7 @@ class IPBlock
     {
         return $this->blocked_range_table;
     }
-    
+
     /**
      * Change the default table name where the ISO Country list is located
      * @param string $table This should be that table name where the ISO list is located
@@ -83,7 +100,7 @@ class IPBlock
         }
         return $this;
     }
-    
+
     /**
      * Returns the blocked ISO database table
      * @return string
@@ -95,14 +112,100 @@ class IPBlock
 
     /**
      * Checks to see if the given IP is Blocked by listing or range
+     * Verified search engine bots are never blocked
      * @param string $ip This should be the IP you are checking if it is blocked
      * @return boolean If the IP is listed will return true else will return false
      */
     public function isIPBlocked($ip)
     {
+        // Never block verified search engine bots
+        if ($this->isVerifiedBot($ip)) {
+            return false;
+        }
         return ($this->isIPBlockedList($ip) || $this->isIPBlockedRange($ip) || $this->isISOBlocked($ip));
     }
-    
+
+    /**
+     * Verifies if an IP belongs to a legitimate search engine bot
+     * Uses reverse DNS lookup followed by forward DNS verification (Google's recommended method)
+     * Results are cached in session to avoid repeated DNS lookups
+     * @param string $ip The IP address to verify
+     * @return boolean True if the IP is a verified search engine bot
+     */
+    public function isVerifiedBot($ip)
+    {
+        // Check session cache first
+        $cacheKey = 'verified_bot_' . md5($ip);
+        if (isset($_SESSION[$cacheKey])) {
+            return $_SESSION[$cacheKey];
+        }
+
+        $isBot = false;
+
+        // Quick check: does the User-Agent claim to be a bot?
+        $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+        if (!$this->looksLikeBot($userAgent)) {
+            $_SESSION[$cacheKey] = false;
+            return false;
+        }
+
+        // Perform reverse DNS lookup
+        $hostname = gethostbyaddr($ip);
+
+        // If reverse DNS fails, gethostbyaddr returns the IP
+        if ($hostname !== $ip) {
+            // Check if hostname matches known bot domains
+            foreach ($this->verifiedBotDomains as $domain) {
+                if (substr($hostname, -strlen($domain)) === $domain) {
+                    // Verify with forward DNS lookup (security check)
+                    $forwardIPs = gethostbynamel($hostname);
+                    if ($forwardIPs && in_array($ip, $forwardIPs)) {
+                        $isBot = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Cache the result in session
+        $_SESSION[$cacheKey] = $isBot;
+
+        return $isBot;
+    }
+
+    /**
+     * Quick check if User-Agent looks like a search engine bot
+     * @param string $userAgent The User-Agent string
+     * @return boolean True if it looks like a bot
+     */
+    protected function looksLikeBot($userAgent)
+    {
+        $botSignatures = [
+            'Googlebot',
+            'Googlebot-Image',
+            'Googlebot-News',
+            'Googlebot-Video',
+            'Mediapartners-Google',
+            'AdsBot-Google',
+            'Google-InspectionTool',
+            'Storebot-Google',
+            'bingbot',
+            'msnbot',
+            'Yahoo! Slurp',
+            'YandexBot',
+            'Baiduspider',
+            'DuckDuckBot',
+        ];
+
+        foreach ($botSignatures as $signature) {
+            if (stripos($userAgent, $signature) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     /**
      * Checks to see if the given IP is Blocked
      * @param string $ip This should be the IP you are checking if it is blocked
@@ -112,7 +215,7 @@ class IPBlock
     {
         return boolval($this->db->select($this->getBlockedIPTable(), ['ip' => $ip]));
     }
-    
+
     /**
      * Checks to see if the given IP is within a blocked range
      * @param string $ip This should be the IP you are checking if it is blocked
@@ -123,7 +226,7 @@ class IPBlock
         $checkIP = ip2long($ip);
         return $this->db->select($this->getBlockedRangeTable(), ['ip_start' => ['<=' => $checkIP], 'ip_end' => ['>=' => $checkIP]]);
     }
-    
+
     /**
      * Check to see if the ISO county of the IP is blocked
      * @param string $ip This should be the IP you are checking if it is blocked
@@ -133,7 +236,7 @@ class IPBlock
     {
         return $this->db->select($this->getBlockedISOTable(), ['iso' => $this->getIPCountryISO($ip)]);
     }
-    
+
     /**
      * Adds an individual IP to the blocked list
      * @param string $ip This should be the IP address that you are blocking
@@ -146,7 +249,7 @@ class IPBlock
         }
         return false;
     }
-    
+
     /**
      * Removes an IP address from the blocked list
      * @param string $ip This should be that IP address that you are removing from the blocked list
@@ -156,7 +259,7 @@ class IPBlock
     {
         return $this->db->delete($this->getBlockedIPTable(), ['ip' => $ip], 1);
     }
-    
+
     /**
      * List all of the IP addresses blocked in the table
      * @return boolean|array An array will be return containing all blocked IP addresses if none exist will return false
@@ -165,7 +268,7 @@ class IPBlock
     {
         return $this->db->selectAll($this->getBlockedIPTable());
     }
-    
+
     /**
      * Adds a IP range to block in the database where any IP between the start and end values should be blocked
      * @param string $start This should be the start of the range you wish to block e.g. 255.255.255.0
@@ -179,7 +282,7 @@ class IPBlock
         }
         return false;
     }
-    
+
     /**
      * Removes a blocked range from the database
      * @param int|boolean $id If you know the ID of the range you wish to remove set this else set to false
@@ -196,7 +299,7 @@ class IPBlock
         }
         return $this->db->delete($this->getBlockedRangeTable(), $where, 1);
     }
-    
+
     /**
      * List all of the IP ranges blocked in the table
      * @return boolean|array An array will be return containing all blocked IP address ranges if none exist will return false
@@ -212,7 +315,7 @@ class IPBlock
         }
         return $ranges;
     }
-    
+
     /**
      * Returns the ISO county of an IP address
      * @param string $ip This should be the IP address you are checking
@@ -230,7 +333,7 @@ class IPBlock
         }
         return false;
     }
-    
+
     /**
      * Add an ISO country to the blocked list
      * @param string $iso This should be the ISO county
@@ -243,7 +346,7 @@ class IPBlock
         }
         return false;
     }
-    
+
     /**
      * Remove an ISO country from the blocked list
      * @param string $iso This should be the ISO county
